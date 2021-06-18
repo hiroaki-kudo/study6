a8a63188a7c360e778ae22a97b77e2957dd9831a
kudo_hiroaki <pikoniconico26@gmail.com>
2021-06-18 15:03:34 +0900

    編集機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,17 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集しました"
+    else
+      render :edit
+    end
+  end
   private
   def post_params
     params.require(:post).permit(:content)
